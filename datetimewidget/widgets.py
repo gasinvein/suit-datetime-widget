951864e8c398593f823967b66487e18db96bd992
gasinvein <gasinvein@gmail.com>
2015-02-24 12:24:44 +0300

    suit template

--- datetimewidget/widgets.py
+++ datetimewidget/widgets.py
@@ -99,6 +99,16 @@ toJavascript_re = re.compile(r'(?<!\w)(' + '|'.join(dateConversiontoJavascript.k
 
 
 BOOTSTRAP_INPUT_TEMPLATE = {
+    'suit': """
+       <div id="%(id)s"  class="input-append date">
+           %(rendered_widget)s
+           %(clear_button)s
+           <span class="add-on"><i class="icon-th"></i></span>
+       </div>
+       <script type="text/javascript">
+           $("#%(id)s").datetimepicker({%(options)s});
+       </script>
+       """,
     2: """
        <div id="%(id)s"  class="controls input-append date">
            %(label)s
